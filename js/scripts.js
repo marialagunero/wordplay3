17ea0358ce03bf1a7d7c909a19c1d843ea993ca7
Maria Lagunero <maria58@gmail.com>
2020-04-05 13:15:40 -0700

    add initial lines for html and js

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#wordplay").submit(function(event) {
+    event.preventDefault();
+
+    var sentenceInput = $("#userInput").val();
+    var sentencseArray = sentenceInput.split(" ");
+
+    
+
+  })
+  console.log(sentenceInput);
+});
